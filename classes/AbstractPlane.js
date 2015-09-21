14f4c3b5305c8626f59e9aaab05217caa172b824
Sergey Shablenko <sergey_shablenko@itservices.in.ua>
2015-09-21 16:55:26 +0300

    minor implementions

--- classes/AbstractPlane.js
+++ classes/AbstractPlane.js
@@ -1,21 +1,22 @@
-var AbstractPlane = function (name, hp, width, height, maxSpeed, acceleration, rotationSpeed) {
+var AbstractPlane = function (name, hp, width, height, maxAcceleration, acceleration, rotationSpeed) {
     this.name = name;
     this.hp = hp;
     this.width = width;
     this.height = height;
-    this.maxSpeed = maxSpeed;
     this.acceleration = acceleration;
+    this.maxAcceleration = maxAcceleration;
     this.rotationMaxSpeed = rotationSpeed;
     this.rotationAcceleration = rotationSpeed;
 
-    this.currentAcceleration = 0;
     this.currentRotationAcceleration = 0;
-    this.direction = 45;
+    this.currentAcceleration = 0;
+    this.currentSpeed = 0;
+    this.direction = 0;
     this.weapon = [];
     this.x = 0;
     this.y = 0;
 };
 
-AbstractPlane.prototype.move = function (direction) {
+AbstractPlane.prototype.move = function (direction, dt) {
 
 };
